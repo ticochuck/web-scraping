76e9e7f23b651006cacdd7e791cbbc1b30139316
ticochuck <chucklivilla@gmail.com>
2020-07-23 19:33:03 -0700

    completed lab17

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,16 +1,43 @@
 import requests
-from bs4 import BeatifulSoup
+from bs4 import BeautifulSoup
 
-def get_citations_needed_count(page_url):
+class wiki_page_parse():
     """
-    Takes in url of a wikipedia page
-    Returns an interger of number of citations needed in the page
+    Main class. Takes in a URL and parses information from the website. 
     """
-    pass
+    def __init__(self, URL):
+        self.url = URL
+        self.response = requests.get(URL)
+        self.content = self.response.content
+        self.soup = BeautifulSoup(self.content, 'html.parser')
 
-def get_citations_needed_report(page_url):
-    """
-    Takes in url of a wikipedia page
-    Returns a formatted string with each citation needed in separete lines, in the order found
-    """
-    pass
+
+    def get_citations_needed_count(self):
+        """
+        Takes in url of a wikipedia page
+        Returns an interger of number of citations needed in the page
+        """
+        
+        citations = self.soup.find_all('a', title = 'Wikipedia:Citation needed')
+        total_citations = len(citations)
+        print(total_citations, 'total citations')
+        return total_citations
+
+
+    def get_citations_needed_report(self):
+        """
+        Takes in url of a wikipedia page
+        Returns a formatted string with each citation needed in separete lines, in the order found
+        """
+        
+        citations = self.soup.find_all('p')
+        paragraphs = ""
+        for p in citations:
+            if p.find('a', title = 'Wikipedia:Citation needed'): 
+                paragraphs += str(p.text) + '\n'
+        print(paragraphs)
+        return paragraphs
+    
+washington= wiki_page_parse("https://en.wikipedia.org/wiki/United_States")
+washington.get_citations_needed_count()
+washington.get_citations_needed_report()
